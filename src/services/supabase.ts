cb18f93054c7f9657e52b0344d5908416ef296ae
SHIS <shis@example.com>
2025-07-08 15:29:32 +0300

    key

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = 'https://ivxpejnqfudtefzisphu.supabase.co' //import.meta.env.VITE_SUPABASE_URL
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2eHBlam5xZnVkdGVmemlzcGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyMDM0NzksImV4cCI6MjA2Njc3OTQ3OX0.kFElH_o0DcQ8oq9GH80Fy6aR9yaLs2vQMdS731MDMVI' //import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
